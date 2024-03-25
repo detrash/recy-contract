84d161d35a2f178da06787f9fc096142a8ca5dd8
neddy34 <13149596+neddy34@users.noreply.github.com>
2024-03-24 21:27:38 -0400

    update contracts

---
+++ contracts/FCWERC721.sol
@@ -0,0 +1,122 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract DeTrashCertificate is
+    ERC721URIStorage,
+    ERC721Enumerable,
+    AccessControl
+{
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    string baseURI;
+    string public baseExtension = ".json";
+
+    error OnlyOneNFTPerAccount();
+
+    constructor() ERC721("DeTrash Certificate", "DeTrash") {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(MINTER_ROLE, _msgSender());
+        _setupRole(BURNER_ROLE, _msgSender());
+    }
+
+    function mint(
+        address recipient
+    ) public onlyRole(MINTER_ROLE) returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+
+        uint256 balance = balanceOf(recipient);
+
+        // Only one NFT per account.
+        if (balance > 0) {
+            revert OnlyOneNFTPerAccount();
+        }
+
+        _mint(recipient, newItemId);
+        _setTokenURI(
+            newItemId,
+            string(abi.encodePacked(recipient, baseExtension))
+        );
+
+        return newItemId;
+    }
+
+    function burnByTokenId(uint256 tokenId) public onlyRole(BURNER_ROLE) {
+        _burn(tokenId);
+    }
+
+    function burn(address user) public onlyRole(BURNER_ROLE) {
+        uint256 tokenId = tokenOfOwnerByIndex(user, 0);
+
+        _burn(tokenId);
+    }
+
+    function setURIOptions(
+        string memory _newBaseURI,
+        string memory _baseExtension
+    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        baseURI = _newBaseURI;
+        baseExtension = _baseExtension;
+    }
+
+    function _transfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual override {
+        // Transfer is disabled
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 firstTokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, firstTokenId, batchSize);
+    }
+
+    function _burn(
+        uint256 tokenId
+    ) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        virtual
+        override(AccessControl, ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return
+            interfaceId == type(IAccessControl).interfaceId ||
+            interfaceId == type(IERC721).interfaceId ||
+            interfaceId == type(IERC721Enumerable).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+}
--- contracts/TimeLock.sol
+++ contracts/TimeLock.sol
@@ -1,387 +1,163 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import {SafeERC20Upgradeable, IERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+// import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-import {CRecy} from "./CRecy.sol";
-import {DeTrashCertificate} from "./ERC721Certificate.sol";
-
-contract TimeLock is Pausable, Ownable, ReentrancyGuard {
-    uint256 public lockPeriod = 2 * 365 days; // 2 years
-    uint256 public emergencyLockPeriod = 365 days; // 1 year
-
-    uint256 public minLockAmount = 100 * 10 ** 18; // 100 cRECY
-
-    CRecy public immutable cRECY;
-    DeTrashCertificate public immutable deTrashCertificate;
+contract TimeLock is
+    Initializable,
+    PausableUpgradeable,
+    OwnableUpgradeable,
+    ReentrancyGuardUpgradeable
+{
+    using SafeERC20Upgradeable for IERC20Upgradeable;
 
     struct Lock {
         uint256 amount;
-        uint256 lockTime;
+        uint256 lockedAt;
         uint256 unlockedAt;
+        bool earlyWithdrawalAllowed;
     }
 
-    // locker(address) => lockIndex => Lock
-    mapping(address => mapping(uint256 => Lock)) private _locks;
-    mapping(address => mapping(uint256 => bool))
-        private _allowedEmergencyUnlocks;
-    mapping(address => uint256) private _lockCount;
-    mapping(uint256 => address) private _lockersByInidex;
-    mapping(address => bool) private _blocked;
+    struct UserLock {
+        uint256 lockCount;
+        uint256 totalLocked;
+        uint256 totalUnlocked;
+        mapping(uint256 => Lock) locks;
+    }
 
-    uint256 private _totalLockers;
-    uint256 private _totalLocked;
-    uint256 private _totalUnlocked;
+    IERC20Upgradeable public cRECY;
 
-    address public signer;
+    uint256 public defaultLockPeriod = 2 * 365 days;
+    uint256 public earlyLockPeriod = 1 * 365 days;
+    uint256 public totalLocked;
+    uint256 public totalUnlocked;
+    uint256 public totalLockers;
 
-    event LockFunds(address indexed owner, uint256 amount, uint256 lockTime);
-    event UnlockFunds(
-        address indexed owner,
-        uint256 amount,
-        uint256 unlockTime
-    );
+    mapping(address => UserLock) private _userLocks;
+    mapping(uint256 => address) private _lockersByIndex;
 
-    error InvalidLockAmount();
-    error InvalidTime();
+    event Locked(address indexed user, uint256 amount, uint256 lockedAt);
+    event Unlocked(address indexed user, uint256 amount, uint256 unlockedAt);
+
+    error InLockPeriod(uint256 unlockTime);
     error AlreadyUnlocked();
-    error Blocked();
-    error InvalidSignature();
 
-    modifier notBlocked() {
-        if (_blocked[msg.sender]) {
-            revert Blocked();
-        }
-        _;
+    function initialize(address _cRECY) external initializer {
+        __Pausable_init();
+        __Ownable_init();
+        __ReentrancyGuard_init();
+
+        cRECY = IERC20Upgradeable(_cRECY);
     }
 
-    constructor(address _cRECY, address _deTrashCertificate) {
-        cRECY = CRecy(_cRECY);
-        deTrashCertificate = DeTrashCertificate(_deTrashCertificate);
+    function lock(uint256 amount) external whenNotPaused nonReentrant {
+        _registerLocker(_msgSender());
+
+        _lock(_msgSender(), amount);
     }
 
-    /**
-     * @dev Lock cRECY token
-     * @param amount amount to lock
-     */
-    function lock(
-        uint256 amount,
-        bytes memory _signature
-    ) external whenNotPaused nonReentrant notBlocked {
-        if (amount < minLockAmount) {
-            revert InvalidLockAmount();
-        }
-        uint256 lockTime = block.timestamp;
-        uint256 lockIndexByUser = _lockCount[msg.sender];
-
-        if (signer != address(0x0)) {
-            // Validate signature
-            bytes32 message = keccak256(
-                abi.encodePacked(lockIndexByUser, amount)
-            );
-            bytes32 messageHash = ECDSA.toEthSignedMessageHash(message);
-            address _signer = ECDSA.recover(messageHash, _signature);
-
-            if (_signer != signer) {
-                revert InvalidSignature();
-            }
-        }
-
-        Lock memory _lock = Lock({
-            amount: amount,
-            lockTime: lockTime,
-            unlockedAt: 0
-        });
-
-        _beforeLock(_lock);
-
-        cRECY.transferFrom(msg.sender, address(this), amount);
-
-        if (lockIndexByUser == 0) {
-            _lockersByInidex[_totalLockers] = msg.sender;
-            ++_totalLockers;
-        }
-
-        _locks[msg.sender][lockIndexByUser] = _lock;
-        ++_lockCount[msg.sender];
-        _totalLocked += amount;
-
-        emit LockFunds(msg.sender, amount, lockTime);
-
-        _afterLock(_lock);
+    function unlock(uint256 lockIndex) external {
+        _unlock(_msgSender(), lockIndex);
     }
 
-    /**
-     * @dev Unlock cRECY token
-     * @param _lockIndex lock index by locker
-     */
-    function unlock(
-        uint256 _lockIndex,
-        bool _emergencyUnlock
-    ) external whenNotPaused nonReentrant notBlocked {
-        if (_emergencyUnlock) {
-            if (!_canEmergencyUnlock(msg.sender, _lockIndex)) {
-                revert InvalidTime();
-            }
-        } else {
-            if (!_canUnlock(msg.sender, _lockIndex)) {
-                revert InvalidTime();
-            }
-        }
-
-        _onUnlock(msg.sender, _lockIndex, _emergencyUnlock);
-        _unlockUnChecked(msg.sender, _lockIndex);
-    }
-
-    /**
-     * @dev Pause the contract
-     */
-    function pause() external onlyOwner {
+    function pause() public onlyOwner {
         _pause();
     }
 
-    /**
-     * @dev Unpause the contract
-     */
-    function unpause() external onlyOwner {
+    function unpause() public onlyOwner {
         _unpause();
     }
 
-    /**
-     * @dev Add or remove user from block list
-     * @param _users The array of addresses representing the users to be added or removed from the block list.
-     * @param _statuses The array of booleans representing the statuses indicating whether to block or unblock the corresponding user.
-     */
-    function setBlocked(
-        address[] memory _users,
-        bool[] memory _statuses
-    ) external onlyOwner {
-        require(_users.length == _statuses.length);
-        uint256 len = _users.length;
-        for (uint i = 0; i < len; ) {
-            _blocked[_users[i]] = _statuses[i];
+    function getUserLocks(
+        address user
+    )
+        public
+        view
+        returns (
+            uint256[] memory lockIndexes,
+            Lock[] memory locks,
+            uint256 _totalLocked,
+            uint256 _totalUnlocked
+        )
+    {
+        uint256 lockCount = _userLocks[user].lockCount;
+
+        locks = new Lock[](lockCount);
+        lockIndexes = new uint256[](lockCount);
+        _totalLocked = _userLocks[user].totalLocked;
+        _totalUnlocked = _userLocks[user].totalUnlocked;
+
+        for (uint256 i = 0; i < lockCount; ) {
+            locks[i] = _userLocks[user].locks[i];
+            lockIndexes[i] = i;
 
             unchecked {
                 ++i;
             }
         }
+
+        return (lockIndexes, locks, _totalLocked, _totalUnlocked);
     }
 
-    /**
-     * @dev Set lock periods
-     * @param _lockPeriod the duration of the lock period in seconds.
-     * @param _emergencyLockPeriod the duration of the emergency lock period in seconds.
-     */
-    function setLockPeriod(
-        uint256 _lockPeriod,
-        uint256 _emergencyLockPeriod
-    ) external onlyOwner {
-        lockPeriod = _lockPeriod;
-        emergencyLockPeriod = _emergencyLockPeriod;
-    }
-
-    /**
-     * @dev Set minimum lock amount
-     * @param _minLockAmount minimum amount to lock
-     */
-    function setMinLockAmount(uint256 _minLockAmount) external onlyOwner {
-        minLockAmount = _minLockAmount;
-    }
-
-    /**
-     * @dev Set the signer address.
-     * @param _signer The address of the signer to be set.
-     */
-    function setSigner(address _signer) external onlyOwner {
-        signer = _signer;
-    }
-
-    /**
-     * @dev Return the last lock of the user.
-     * @param user The address of the user to query.
-     */
-    function getUserLastLock(address user) public view returns (Lock memory) {
-        return _locks[user][_lockCount[user] - 1];
-    }
-
-    /**
-     * @dev Return the lock count of the user.
-     * @param user The address of the user to query.
-     */
-    function getUserTotalLockCount(address user) public view returns (uint256) {
-        return _lockCount[user];
-    }
-
-    /**
-     * @dev Return the pending locks of the user.
-     * @param user The address of the user to query.
-     */
-    function getUserPendingLockIndexes(
-        address user
-    ) public view returns (uint256[] memory, Lock[] memory) {
-        uint256 len = _lockCount[user];
-        uint256 pendingLocksCount;
-        for (uint256 i = 0; i < len; i++) {
-            if (_locks[user][i].unlockedAt == 0) {
-                ++pendingLocksCount;
-            }
+    function _registerLocker(address _user) private {
+        if (_userLocks[_user].lockCount == 0) {
+            _lockersByIndex[totalLockers] = _user;
+            ++totalLockers;
         }
-        Lock[] memory locks = new Lock[](pendingLocksCount);
-        uint256[] memory lockIndexes = new uint256[](pendingLocksCount);
-        uint256 j;
-        for (uint256 i = 0; i < len; i++) {
-            if (_locks[user][i].unlockedAt == 0) {
-                locks[j] = _locks[user][i];
-                lockIndexes[j] = i;
-                ++j;
-            }
-        }
-        return (lockIndexes, locks);
     }
 
-    /**
-     * @dev Return the locks of the user.
-     * @param user The address of the user to query.
-     */
-    function getUserLocks(address user) public view returns (Lock[] memory) {
-        uint256 len = _lockCount[user];
-        Lock[] memory locks = new Lock[](len);
-        for (uint256 i = 0; i < len; i++) {
-            locks[i] = _locks[user][i];
-        }
-        return locks;
+    function _lock(address _user, uint256 _amount) private {
+        cRECY.safeTransferFrom(_user, address(this), _amount);
+        uint256 lockedAt = block.timestamp;
+        Lock memory lockToAdd = Lock({
+            amount: _amount,
+            lockedAt: lockedAt,
+            unlockedAt: 0,
+            earlyWithdrawalAllowed: false
+        });
+
+        uint256 lockIndex = _userLocks[_user].lockCount;
+        _userLocks[_user].locks[lockIndex] = lockToAdd;
+        _userLocks[_user].totalLocked += _amount;
+        ++_userLocks[_user].lockCount;
+
+        totalLocked += _amount;
+
+        emit Locked(_user, _amount, lockedAt);
     }
 
-    /**
-     * @dev Return the lock of the user.
-     * @param user The address of the user to query.
-     * @param index The index of the lock
-     */
-    function getUserLockByIndex(
-        address user,
-        uint256 index
-    ) public view returns (Lock memory) {
-        return _locks[user][index];
-    }
+    function _unlock(address _user, uint256 _lockIndex) private {
+        Lock memory userLock = _userLocks[_user].locks[_lockIndex];
 
-    /**
-     * @dev Return the locker by index.
-     * @param index The index of the locker to query.
-     */
-    function getLockerByIndex(uint256 index) public view returns (address) {
-        return _lockersByInidex[index];
-    }
-
-    /**
-     * @dev Checks if a user can unlock a specific lock.
-     * @param user The address of the user to check.
-     * @param lockIndex The index of the lock to check.
-     * @return A boolean indicating whether the user can unlock the lock.
-     */
-    function canUnlock(
-        address user,
-        uint256 lockIndex
-    ) public view returns (bool) {
-        return _canUnlock(user, lockIndex);
-    }
-
-    /**
-     * @dev Checks if an emergency unlock can be performed by a user for a specific lock.
-     * @param user The address of the user to check.
-     * @param lockIndex The index of the lock to check.
-     * @return A boolean indicating whether the user can perform an emergency unlock.
-     */
-    function canEmergencyUnlock(
-        address user,
-        uint256 lockIndex
-    ) public view returns (bool) {
-        return _canEmergencyUnlock(user, lockIndex);
-    }
-
-    /**
-     * @dev Returns the total number of lockers.
-     * @return The total number of lockers.
-     */
-    function getTotalLockers() public view returns (uint256) {
-        return _totalLockers;
-    }
-
-    /**
-     * @dev Returns the total locked cRECY amount.
-     * @return The total locked cRECY amount.
-     */
-    function getTotalLocked() public view returns (uint256) {
-        return _totalLocked;
-    }
-
-    /**
-     * @dev Returns the total unlocked cRECY amount.
-     * @return The total unlocked cRECY amount.
-     */
-    function getTotalUnlocked() public view returns (uint256) {
-        return _totalUnlocked;
-    }
-
-    function _unlockUnChecked(address _user, uint256 _lockIndex) internal {
-        if (_locks[_user][_lockIndex].unlockedAt > 0) {
+        if (userLock.unlockedAt != 0) {
             revert AlreadyUnlocked();
         }
 
-        uint256 lockAmount = _locks[_user][_lockIndex].amount;
-
-        cRECY.transfer(_user, lockAmount);
-
-        uint256 unlockTime = block.timestamp;
-
-        _locks[_user][_lockIndex].unlockedAt = unlockTime;
-        _totalLocked -= lockAmount;
-        _totalUnlocked += lockAmount;
-
-        emit UnlockFunds(_user, lockAmount, unlockTime);
-    }
-
-    function _beforeLock(Lock memory _lock) internal {}
-
-    function _afterLock(Lock memory _lock) internal {
-        uint256 balance = deTrashCertificate.balanceOf(_msgSender());
-
-        if (balance == 0) {
-            deTrashCertificate.mint(_msgSender());
+        uint256 unlockTime;
+        if (userLock.earlyWithdrawalAllowed) {
+            unlockTime = userLock.lockedAt + earlyLockPeriod;
+        } else {
+            unlockTime = userLock.lockedAt + defaultLockPeriod;
         }
-    }
-
-    function _onUnlock(
-        address _user,
-        uint256 _lockIndex,
-        bool emergencyUnlock
-    ) internal {
-        if (emergencyUnlock) {
-            uint256 balance = deTrashCertificate.balanceOf(_user);
-
-            if (balance > 0) {
-                deTrashCertificate.burn(_user);
-            }
+        if (unlockTime < block.timestamp) {
+            revert InLockPeriod(unlockTime);
         }
-    }
 
-    function _canUnlock(
-        address _user,
-        uint256 _lockIndex
-    ) internal view returns (bool) {
-        return
-            _locks[_user][_lockIndex].lockTime + lockPeriod < block.timestamp;
-    }
+        _userLocks[_user].locks[_lockIndex].unlockedAt = block.timestamp;
+        _userLocks[_user].totalLocked -= userLock.amount;
+        _userLocks[_user].totalUnlocked += userLock.amount;
 
-    function _canEmergencyUnlock(
-        address _user,
-        uint256 _lockIndex
-    ) internal view returns (bool) {
-        return
-            _allowedEmergencyUnlocks[msg.sender][_lockIndex] &&
-            _locks[_user][_lockIndex].lockTime + emergencyLockPeriod <
-            block.timestamp;
+        totalLocked -= userLock.amount;
+        totalUnlocked += userLock.amount;
+
+        cRECY.safeTransfer(_user, userLock.amount);
+
+        emit Unlocked(_user, userLock.amount, block.timestamp);
     }
 }
