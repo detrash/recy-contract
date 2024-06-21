1a9a82ccf67d11132ecfdc5851f9fba810ae43a7
neddy34 <13149596+neddy34@users.noreply.github.com>
2024-06-21 23:56:54 +0900

    not working test

--- test/TimeLock.ts
+++ test/TimeLock.ts
@@ -1,20 +1,42 @@
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
+
+import { TimeLock } from "../typechain-types";
+import { expect } from "chai";
 
 describe("TimeLock", function () {
   async function deployFixture() {
-    const TimeLock = await ethers.getContractFactory("TimeLock");
+    const TimeLock__factory = await ethers.getContractFactory("TimeLock");
     const CRecy = await ethers.getContractFactory("CRecy");
-    const DeTrashCertificate = await ethers.getContractFactory(
-      "DeTrashCertificate"
-    );
+    const FCWERC721__factory = await ethers.getContractFactory("FCWERC721");
 
     const cRECY = await CRecy.deploy(1000, 1000);
-    const deTrashCertificate = await DeTrashCertificate.deploy();
-    const timeLock = await TimeLock.deploy(
+
+    const deTrashCertificate = await FCWERC721__factory.deploy();
+    const timeLock = (await upgrades.deployProxy(TimeLock__factory, [
       cRECY.address,
-      deTrashCertificate.address
-    );
+    ])) as TimeLock;
 
     return { cRECY, deTrashCertificate, timeLock };
   }
+
+  it("should deploy contracts", async () => {
+    await deployFixture();
+  });
+
+  it("should lock token", async () => {
+    const { cRECY, timeLock } = await deployFixture();
+
+    await cRECY.approve(timeLock.address, 500);
+    await timeLock.lock(500);
+  });
+
+  it("should unlock token", async () => {
+    const { cRECY, timeLock } = await deployFixture();
+
+    await cRECY.approve(timeLock.address, 500);
+    await timeLock.lock(500);
+
+    // TODO
+    // await expect(timeLock.unlock(500)).to.be.revertedWith(`AlreadyListed`);
+  });
 });
